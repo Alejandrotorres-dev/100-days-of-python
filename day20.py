dbe622b1478ce9e6c8e6cb8d8c8ac126607b6f98
Alejandrotorres-dev <alejandrowindows2025@gmail.com>
2025-06-03 09:27:41 +0200

    Add files via upload

---
+++ day20.py
@@ -0,0 +1,11 @@
+
+i1= int(input("Introduce un valor minimo: "))
+i2 = int(input("Introduce un valor maximo: "))
+increment = int(input("Incremento"))
+for i in range (i1,i2,increment):
+    print(i)
+
+
+
+
+    
