6eeeb6562d757805dff2bb2fac5a58b86bb0fdad
Alejandrotorres-dev <alejandrowindows2025@gmail.com>
2025-06-11 11:36:22 +0200

    Add files via upload

---
+++ day23_loginsystem.py
@@ -0,0 +1,17 @@
+print("Login system")
+
+correct_username = "alejandro"
+correct_password = "buenas"
+
+def login():
+    while True:
+        username = input("What is your username?: ")
+        password = input("What is your password?: ")
+        if username == correct_username and password == correct_password:
+            print("Welcome to replit!")
+            break
+        else:
+            print("Incorrect, try again")
+            continue
+
+login()
