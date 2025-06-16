02997cf618546528b114b7f809944de2e8a587fd
Alejandrotorres-dev <alejandrowindows2025@gmail.com>
2025-06-16 13:03:02 +0200

    Add files via upload

---
+++ sumandproduct.py
@@ -0,0 +1,6 @@
+number1 = int(input("Please write the first number: "))
+number2 = int(input("Please write the second number: "))
+
+print(f"The sum of the numbers:{number1 + number2}")
+
+print(f"The product of the numbers:{number1 * number2}")
