0e6ea92213e95869af4d4b24db80798e5f9f0b83
Alejandrotorres-dev <alejandrowindows2025@gmail.com>
2025-06-24 09:15:38 +0200

    Add files via upload

---
+++ basen.py
@@ -0,0 +1,11 @@
+limit = int(input("Upper limit: ")) #27
+
+base = int(input("Base: ")) #3
+
+number = 1
+
+
+
+while number<=limit:
+   print(number)
+   number= number * base
---
+++ power2.py
@@ -0,0 +1,7 @@
+limit = int(input("Upper limit: "))
+number = 1
+while number <= limit:
+    print(number)
+    number *= 2
+
+#En cuanto number sea mayhor que limit, la condición se vuelve falsa y el bucle termina.
